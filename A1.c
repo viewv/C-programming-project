146f310c93d1e0b1f55117062ee4b3ac5366c999
ViewV <zxn@zxnnet.top>
2018-03-15 10:32:28 +0800

    wula

--- A1.c
+++ A1.c
@@ -9,20 +9,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int copyFile(char *fileRead, char *fileWrite);
+int copyFile(char *Read, char *Write);
 int main()
 {
-    char fileRead[100];  // 要复制的文件名
-    char fileWrite[100]; // 复制后的文件名
+    char Read[100];  // 要复制的文件名
+    char Write[100]; // 复制后的文件名
 
     // 获取用户输入
     printf("recourse file name");
-    scanf("%s", fileRead);
+    scanf("%s", Read);
     printf("target filename");
-    scanf("%s", fileWrite);
+    scanf("%s", Write);
 
     // 进行复制操作
-    if (copyFile(fileRead, fileWrite))
+    if (copyFile(Read, Write))
     {
         printf("Successful\n");
     }
@@ -35,31 +35,32 @@ int main()
 
 /**
 * 文件复制函数
-* @param    fileRead    要复制的文件
-* @param    fileWrite   复制后文件的保存路径
-* @return   int         1: 复制成功；2: 复制失败
+* Read     要复制的文件
+* Write    复制后文件的保存路径
+* return   1: 复制成功；2: 复制失败
 **/
-int copyFile(char *fileRead, char *fileWrite)
+int copyFile(char *Read, char *Write)
 {
-    FILE *fpRead;  // 指向要复制的文件
-    FILE *fpWrite; // 指向复制后的文件
+    FILE *fpRead=NULL;  // 指向要复制的文件
+    FILE *fpWrite=NULL; // 指向复制后的文件
 
     /**
 	* 这里设置缓存区，
-	* 1 如果直接读入，如果源文件非常大，内存无法承受
-	* 2 现在的硬盘大多数是4k对齐，这里设置4k的缓存区可以加快速度
+	* 现在的硬盘大多数是4k对齐，这里设置4k的缓存区可以加快速度
 	**/
     int bufferLen = 1024 * 4;                 // 缓冲区长度
     char *buffer = (char *)malloc(bufferLen); // 开辟缓存
     int readCount;                            // 实际读取的字节数
-    if ((fpRead = fopen(fileRead, "rb")) == NULL || (fpWrite = fopen(fileWrite, "wb")) == NULL)
+    if ((fpRead = fopen(Read, "rb")) == NULL || (fpWrite = fopen(Write, "wb")) == NULL)
+    //为了能够复制所有类型文件，这里使用"rb","wb"也就是二进制模式
+    //虽然题目只要求复制文本文件，但是本质上都一样
     {
         printf("Cannot open file, press any key to exit!\n");
         getchar();
         exit(1);
     }
 
-    // 不断从fileRead读取内容，放在缓冲区，再将缓冲区的内容写入fileWrite
+    // 不断从Read读取内容，放在缓冲区，再将缓冲区的内容写入Write
     while ((readCount = fread(buffer, 1, bufferLen, fpRead)) > 0)
     {
         fwrite(buffer, readCount, 1, fpWrite);
---
+++ A2.c
@@ -0,0 +1,102 @@
+/**
+*题号 A2
+*题目内容：
+*删除注释
+*个人姓名：张学楠
+*学号：201700800398
+*班级：2017计算机（普）
+**/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*声明函数*/
+void check_comment(char); // 检查注释函数
+void block_comment();     // 跳过多行注释
+void single_comment();    // 跳过单行注释
+
+FILE *from, *target; //from源文件，target目标文件
+
+int main() //主函数
+{
+    char c;
+    char filename[100];       //源文件名
+    char targetfilename[100]; //目标文件
+
+    //用户输入
+    printf("Please enter the resource filename\n");
+    scanf("%s", filename);
+    printf("Please enter the target filename\n");
+    scanf("%s", targetfilename);
+
+    from = fopen(filename, "r");
+    target = fopen(targetfilename, "w");
+
+    while ((c = fgetc(from)) != EOF) //逐字符输入
+        check_comment(c);            // 检查是否为注释
+
+    //关闭所有文件
+    fclose(from);
+    fclose(target);
+    return 0;
+}
+
+//检查注释函数
+void check_comment(char c)
+{
+    char d;
+    if (c == '/')
+    {
+        if ((d = fgetc(from)) == '*') //找到/*注释头
+        {
+            block_comment(); //跳过多行注释函数
+        }
+
+        else if (d == '/')
+        {
+            single_comment(); //跳过单行注释
+            fputc('\n', target);
+        }
+        else
+        {
+            // 将不是注释的部分写入target文件
+            fputc(c, target);
+            fputc(d, target);
+        }
+    }
+    // 如果都不是，要把c字符写入文件
+    else
+        fputc(c, target);
+}
+
+// 跳过多行注释函数
+void block_comment()
+{
+    char d, e;
+    while ((d = fgetc(from)) != EOF) //读文件，这里是在找到注释头开始读
+    {
+        if (d == '*') // 检查是否到达注释尾
+        {
+            e = fgetc(from);
+            if (e == '/')
+                return;
+        }
+        if (d == '/' && e == '*')
+        {
+            return;
+        }
+    }
+}
+
+// 跳过单行注释函数
+void single_comment()
+{
+    char d, e;
+    while ((d = fgetc(from)) != EOF) //读文件，这里是在单行注释头之后读取
+    {
+        if (d == '\n') // 检查是否到注释尾
+        {
+            return;
+        }
+    }
+}
